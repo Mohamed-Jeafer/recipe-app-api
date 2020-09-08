50ec673ec31c1e43a9e1714658d43ae394474829
Mohamed Jeafer <34100676+Mohamed-Jeafer@users.noreply.github.com>
2020-09-09 00:08:07 +0400

    added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for the user object"""
+    """Serializer for the users object"""
 
     class Meta:
         model = get_user_model()
@@ -13,16 +13,27 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
-        """create a new user with encrypted password and return it"""
+        """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
-    """Serializer for user authentication object"""
+    """Serializer for the user authentication object"""
     email = serializers.CharField()
     password = serializers.CharField(
         style={'input_type': 'password'},
-        trim_whitespace=False,
+        trim_whitespace=False
     )
 
     def validate(self, attrs):
